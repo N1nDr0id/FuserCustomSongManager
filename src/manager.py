62c8d1b2145df656783d09493cc941cd27492001
N1nDr0id <38891169+N1nDr0id@users.noreply.github.com>
2023-11-27 23:13:35 -0600

    Compiled changes

--- src/manager.py
+++ src/manager.py
@@ -736,7 +736,7 @@ def clear_search(clear_button):
     clear_button.configure(state='disabled')
 
 def show_about():
-    version_number = "v0.8"
+    version_number = "v0.8.1"
 
     popup = tk.Toplevel()
     popup.focus_force()
