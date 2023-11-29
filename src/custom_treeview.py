fad0b78b2c72a43858c41cc3cb4e0630866f4647
N1nDr0id <38891169+N1nDr0id@users.noreply.github.com>
2023-11-28 20:19:32 -0600

    Changed song name & artist sort to work like Fuser

--- src/custom_treeview.py
+++ src/custom_treeview.py
@@ -5,6 +5,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from functools import partial
+import list_ops
 
 class MyTreeview(ttk.Treeview):
     def heading(self, column, sort_by=None, **kwargs):
@@ -15,9 +16,25 @@ class MyTreeview(ttk.Treeview):
         return super().heading(column, **kwargs)
 
     def _sort(self, column, reverse, data_type, callback):
-        l = [(self.set(k, column), k) for k in self.get_children('')]
+        # Handles case where "The " and "A " should be ignored from the title of a song or artist name, similar to how Fuser itself handles sorting by song or artist name.
+        if (data_type == "remove_the_a"):   
+            # songs that start with "the"
+            songs_the = [(self.set(k, column).casefold().replace('the ', '', 1), k) for k in self.get_children('') if self.set(k, column).casefold().startswith("the ")]
+            # songs that start with "a"
+            songs_a = [(self.set(k, column).casefold().replace('a ', '', 1), k) for k in self.get_children('') if self.set(k, column).casefold().startswith("a ")]
+            # songs that simultaneously don't start with the and a
+            songs_not_the_a =[(self.set(k, column).casefold(), k) for k in self.get_children('') if (not self.set(k, column).casefold().startswith("a ") and not self.set(k, column).casefold().startswith("the "))]
+            # all songs, with the first occurances of "the " and "a " removed from their starts, depending on which one comes first
+            l = list_ops.union(list_ops.union(songs_a, songs_the), songs_not_the_a)
+            data_type = str.casefold
+        else:
+            l = [(self.set(k, column), k) for k in self.get_children('')]
+        
+        #print(l)
         l.sort(key=lambda t: data_type(t[0]), reverse=reverse)
+        #print("SORT")
         for index, (_, k) in enumerate(l):
+            #print(_, k)
             self.move(k, '', index)
         self.heading(column, command=partial(callback, column, not reverse))
 
@@ -26,3 +43,6 @@ class MyTreeview(ttk.Treeview):
 
     def _sort_by_name(self, column, reverse):
         self._sort(column, reverse, str.casefold, self._sort_by_name)
+
+    def _sort_by_name_ignore_words(self, column, reverse):
+        self._sort(column, reverse, "remove_the_a", self._sort_by_name_ignore_words)
